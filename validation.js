ab558ab45aae4100585f058a10a4b6fdd5fa44f7
ttpatel <54196748+ttpatel@users.noreply.github.com>
2021-02-07 13:12:44 +0530

    1:11

--- validation.js
+++ validation.js
@@ -1,60 +1,59 @@
-function validate(selector, regex){
-    if(regex.test(selector.value)){
+function validate(selector, regex) {
+    if (regex.test(selector.value)) {
         return true;
     }
-    return false;
+   return false;
+}
+function checkIfIsInvalid(selector,regex){
+selector.addEventListener('keyup', (e) => {
+   
+    if (validate(selector, regex)) {
+        selector.classList.remove('is-invalid');
+    }
+   
+})
 }
 
-function checkIfIsInvalid(selector) {
-    selector.addEventListener('keyup',(e) => {
-        const regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
-        if (validate(selector, regex))  {
-            selector.classList.remove('is-invalid');
-        }
-    })
-}
 
+document.getElementById('submit').addEventListener('click', (e) => {
+    //console.log('onSubmit');
 
-document.getElementById('submit').addEventListener('click',(e) =>{
-    console.log('onSubmit');
     e.preventDefault();
     const firstName = document.getElementById('fname');
-    
     const lastName = document.getElementById('lname');
-    
+    const emailRegex = document.getElementById('email');
+    const mobileNumber=document.getElementById('phone');
+    const userAddress=document.getElementById('address1');
+
+
     const regex = /^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
+    const regex1 = /^([_\-\.0-9a-zA-Z]+)@([_\-\.0-9a-zA-Z]+)\.([a-zA-Z ]){0,3}$/;
+     const regex2 = /^([0-9]){10}$/;
+     const regex3=/^[\s_\-\.\,\""\&0-9a-zA-Z]([\s_\-\.\,\""\&0-9a-zA-Z]){2,100}$/;
     if (!validate(firstName, regex)) {
         firstName.classList.add('is-invalid');
-        checkIfIsInvalid(firstName);
+        checkIfIsInvalid(firstName,regex);
     }
     if (!validate(lastName, regex)) {
         lastName.classList.add('is-invalid');
-        checkIfIsInvalid(lastName);
+        checkIfIsInvalid(lastName,regex);
     }
+    if (!validate(emailRegex, regex1)) {
+        emailRegex.classList.add('is-invalid');
+        checkIfIsInvalid(emailRegex,regex1);
+    }
+    if (!validate(mobileNumber, regex2)) {
+        mobileNumber.classList.add('is-invalid');
+        checkIfIsInvalid(mobileNumber,regex2);
+    }
+    if (!validate(userAddress, regex3)) {
+        userAddress.classList.add('is-invalid');
+        checkIfIsInvalid(userAddress,regex3);
+    }
+    
 });
 
 
 
 
-// var firstname=/^[a-zA-Z]([0-9a-zA-Z]){2,10}$/;
 
-
-// function validate(form){
-//     var firstname=getElementById('fname').value;
-// }
-
-// let submit = document.getElementById('submit');
-// submit.addEventListener('click', (e) => {
-//     e.preventDefault();
-
-//     console.log('you clicked on submit');
-
-//     if(!firstname.test(fname)){
-//         errors[errors.length] = "not valid Name .";
-//     }
-
-// })
-
-// function error(errors){
-//     var msg="please enter valid data"
-// }
